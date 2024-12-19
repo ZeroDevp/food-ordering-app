d279c54862ec79e05dc0a131346d3836191ff375
Nguyen Trong Hieu <hieunguyen6231@gmail.com>
2024-12-19 23:00:18 +0700

    fix bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,44 @@
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+// const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+// module.exports = {
+//     optimization: {
+//         minimize: true,
+//         minimizer: [
+//             '...', // Để các plugin khác như TerserPlugin hoạt động bình thường
+//             new CssMinimizerPlugin({
+//                 minimizerOptions: {
+//                     preset: [
+//                         'default',
+//                         {
+//                             discardComments: { removeAll: true }, // Xóa toàn bộ comment
+//                             normalizeUrl: false, // Ngăn chặn lỗi xử lý URL
+//                         },
+//                     ],
+//                 },
+//             }),
+//         ],
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.css$/,
+//                 use: [
+//                     'style-loader',
+//                     {
+//                         loader: 'css-loader',
+//                         options: {
+//                             url: false, // Vẫn giữ `url: false`
+//                         },
+//                     },
+//                 ],
+//             },
+//         ],
+//     },
+// };
 
 module.exports = {
     optimization: {
-        minimize: true,
-        minimizer: [
-            '...', // Để các plugin khác như TerserPlugin hoạt động bình thường
-            new CssMinimizerPlugin({
-                minimizerOptions: {
-                    preset: [
-                        'default',
-                        {
-                            discardComments: { removeAll: true }, // Xóa toàn bộ comment
-                            normalizeUrl: false, // Ngăn chặn lỗi xử lý URL
-                        },
-                    ],
-                },
-            }),
-        ],
+        minimize: false, // Tắt tối ưu hóa
     },
     module: {
         rules: [
@@ -27,7 +49,7 @@ module.exports = {
                     {
                         loader: 'css-loader',
                         options: {
-                            url: false, // Vẫn giữ `url: false`
+                            url: false, // Vẫn giữ `url: false` nếu cần
                         },
                     },
                 ],
